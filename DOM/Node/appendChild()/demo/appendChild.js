2c47c76c15f00f50890fa9eebe84255b75ffb455
jserz <rnjnfns8@126.com>
2016-12-30 17:56:09 +0800

    给appendChild()方法添加demo

---
+++ DOM/Node/appendChild()/demo/appendChild.js
@@ -0,0 +1,8 @@
+/**
+ * Created by jszhou on 2016/12/30.
+ */
+var wrap = document.querySelector('.js-wrap'),
+    newP = document.createElement('p');
+
+newP.textContent = 'jszhou';
+wrap.appendChild(newP);
