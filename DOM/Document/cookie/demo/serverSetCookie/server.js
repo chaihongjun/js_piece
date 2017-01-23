f9ed08feb7da6f8cde36024ee53fc158dd0bff8e
jserz <rnjnfns8@126.com>
2017-01-23 12:44:44 +0800

    完善cookie

---
+++ DOM/Document/cookie/demo/serverSetCookie/server.js
@@ -0,0 +1,15 @@
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 80;
+
+const server = http.createServer(function (req, res) {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Set-Cookie',['name=zhou; path=/img', 'status=active']);
+  res.end('Hello World\n');
+});
+
+server.listen(port, hostname, function() {
+  console.log('关闭所有服务器软件，打开网址：http://jsz.cn/');
+});
